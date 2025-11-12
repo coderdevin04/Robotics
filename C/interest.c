2ea78a08c739f0337c5ac4070df6b7bdedc53c6f
Devin <jitusaini04@gmail.com>
2025-11-12 21:42:37 +0530

    changes

---
+++ C/interest.c
@@ -0,0 +1,35 @@
+/* Author Devin Date: 12/11/2025 */
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    /*int p, n;
+    float r, si;
+    char t = 'F';
+    int x=70;
+
+    printf("Enter the Values of P, N, R\n");
+    // scanf("%d %d %f", &p, &n, &r);
+
+    si = p * n * r / 100;
+
+    // printf("Simple Interest is %f", si);
+    // printf("Simple Interest is %d", 5 % 2);
+    printf("%d", t);
+    printf("%c", x);
+
+    int a = pow(5, 3);
+    printf("%d\n", a);
+    
+
+    int i = 2, j = 3, k, l;
+    float a, b;
+    k = i / j * j;
+    l = j / i * i;
+    a = i / j * j;
+    b = j / i * i;
+    printf("%d %d %f %f", k, l, a, b);
+    */
+    return 0;
+}
---
+++ C/main.c
@@ -0,0 +1,8 @@
+// Author Devin Date 12/11/2025
+#include<stdio.h>
+
+
+int main (){
+    printf("I'M Back");
+    return 0 ;
+}
