1ae3b019f41374c090e06ad6f8e30affe8734356
Devin <jitusaini04@gmail.com>
2025-11-14 07:58:04 +0530

    changes

---
+++ C/Chapter 2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   
+    int num ;
+
+    printf("Enter a number less than 100 \n");
+    scanf("%d",&num);
+
+    if ( num < 100)
+    printf("Yo! Good Boy");
+    else 
+    printf("You're a bad Boy");
+     /*
+    if (05)
+        printf("Yo! Good Boy");
+    */
+    return 0;
+}
